1e4f99798d32ef958fab5da6682142a548bb7dc1
rrdarkrtg2-code <rrdarkrtg2@gmail.com>
2026-09-16 20:26:39 +0000

    Fix missing imports

--- app/src/main/java/com/jarvis/assistant/MainActivity.kt
+++ app/src/main/java/com/jarvis/assistant/MainActivity.kt
@@ -32,6 +32,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
